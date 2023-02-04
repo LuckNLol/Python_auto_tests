ce4d2b76b7af152536e2d4ce3140ebe6c781bb5d
Vladimir N <savonciel@yandex.ru>
2023-02-04 13:30:40 +0300

    Commit new fin.project

---
+++ Section4/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption("--Mylanguage", action="store", default="en", help="Choose language: ru/en/...(etc)")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("Mylanguage")
+
+    chrome_options = ChromeOptions()
+    chrome_options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+    print("\nstart Chrome browser for test..")
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+
+    browser.implicitly_wait(10)
+    browser.maximize_window()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
